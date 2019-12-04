be4957b236fbfa0ef0b045de5ec65623a78c10eb
Stuart Sewell <hello@stuartsewell.dev>
2019-12-04 11:50:20 +0000

    style framework added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
