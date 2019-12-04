21ee286fd8a74c0fe3b1b1a4bf8193af32cc156f
Stuart Sewell <hello@stuartsewell.dev>
2019-12-04 11:44:03 +0000

    express setup

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+http.listen(port, console.log("Listening on port " + port));
